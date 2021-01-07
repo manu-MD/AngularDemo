8875efef00e8b6aa51f29277e2814f7274fc779a
KAM <k.ziane@mesdocteurs.com>
2021-01-07 18:31:04 +0100

    add auth.guard.ts

---
+++ src/app/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard {
+
+    constructor(
+        private router: Router
+    ) {
+    }
+
+
+}
+
+
